858fc8c5277df4352fd1a2d98ae9672800076e7f
grypis <grypisnecros@gmail.com>
2025-01-12 01:45:14 +0200

    olsad

--- js/localstorage.js
+++ js/localstorage.js
@@ -67,3 +67,94 @@ try {
 } */
 
 //! ==========================================
+/* localStorage.setItem('param8', 'hello param 8');
+localStorage.setItem('userName', 'Volodka') */
+
+/* const user = {
+  name: '123123',
+  age: '123',
+  friend: [12, 12, 3, 1, 2, 4, 1],
+};
+const jsonUser = JSON.stringify(user);
+console.log(jsonUser);
+localStorage.setItem('user', jsonUser); */
+
+//! ==========================================
+/* const data = localStorage.getItem('user');
+const user = JSON.parse(data);
+console.log(data);
+console.log(user); */
+
+//! ==========================================
+// localStorage.removeItem('param8');
+
+//! ==========================================
+const user1 = {
+  name: '123123',
+  age: '123',
+  friend: [12, 12, 3, 1, 2, 4, 1],
+};
+const user2 = {
+  name: '123123',
+  age: '123',
+  friend: [12, 12, 3, 1, 2, 4, 1],
+};
+const user3 = {
+  name: '123123',
+  age: '123',
+  friend: [12, 12, 3, 1, 2, 4, 1],
+};
+const user4 = {
+  name: '123123',
+  age: '123',
+  friend: [12, 12, 3, 1, 2, 4, 1],
+};
+const user5 = {
+  name: '123123',
+  age: '123',
+  friend: [12, 12, 3, 1, 2, 4, 1],
+};
+
+/* localStorage.setItem('user1', JSON.stringify(user1));
+localStorage.setItem('user2', JSON.stringify(user2));
+localStorage.setItem('user3', JSON.stringify(user3));
+localStorage.setItem('user4', JSON.stringify(user4));
+localStorage.setItem('user5', JSON.stringify(user5)); */
+
+/* saveToLS('user1', user1);
+saveToLS('user2', user2);
+saveToLS('user3', user3);
+saveToLS('user4', user4);
+saveToLS('user5', user5); */
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+//! ==========================================
+const data = localStorage.getItem('user1');
+try {
+  const user = JSON.parse(data);
+  console.log(user);
+} catch (err) {
+  console.log(err);
+}
+
+const data1 = loadFromLS('user1');
+const data2 = loadFromLS('user2');
+const data3 = loadFromLS('user3');
+const data4 = loadFromLS('user4');
+const data5 = loadFromLS('user5');
+
+console.log(data2);
+
+function loadFromLS(key) {
+  const json = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(json);
+    return data;
+  } catch {
+    return json;
+  }
+}
