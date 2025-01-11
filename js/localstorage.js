be83144a786dc544abf6e097eb96651d8170e836
grypis <grypisnecros@gmail.com>
2025-01-11 23:45:14 +0200

    ols

---
+++ js/localstorage.js
@@ -0,0 +1,69 @@
+//! ==========================================
+/* const user = {
+  age: 10,
+  name: 'Volodka',
+  city: 'Dnipro',
+};
+
+const zip = JSON.stringify(user);
+console.log(zip); */
+
+//! ==========================================
+/* const str = `{"update_id": 936365413,"message": {"message_id": 1702107,
+   "from": {
+    "id": 433982686,
+    "is_bot": false,
+    "first_name": "Volodymyr",
+    "username": "pashchenko_v_r",
+    "language_code": "uk"
+   },
+   "chat": {
+    "id": 433982686,
+    "first_name": "Volodymyr",
+    "username": "pashchenko_v_r",
+    "type": "private"
+   },
+   "date": 1717002831,
+   "text": "message"
+  }
+ }`;
+
+const data = JSON.parse(str);
+console.log(data); */
+
+//! ==========================================
+//* JSON.stringify(data);
+
+/* const x = [1, 23, 4, 5, 2, 2, undefined, 12, 3, 4];
+const data = JSON.stringify(x);
+console.log(x, data); */
+
+//! ==========================================
+//* JSON.parse(jsonStr)
+
+/* const str = prompt('Enter your json');
+const data = JSON.parse(str);
+console.log(data); */
+
+//! ==========================================
+/* console.log('start');
+
+try {
+  console.log(x);
+} catch (error) {
+  console.log(error);
+}
+
+console.log('end'); */
+
+//! ==========================================
+/* const str = prompt('Enter your json');
+
+try {
+  const data = JSON.parse(str);
+  console.log(data);
+} catch (err) {
+  console.log(err);
+} */
+
+//! ==========================================
